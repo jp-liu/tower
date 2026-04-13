5b42e1c51d7604215c8466009d37ab209225403b
jpliu6 <jpliu6@iflytek.com>
2026-04-13 09:32:29 +0800

    fix: Select dropdown appears below trigger (alignItemWithTrigger=false, align=start)

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -68,9 +68,9 @@ function SelectContent({
   children,
   side = "bottom",
   sideOffset = 4,
-  align = "center",
+  align = "start",
   alignOffset = 0,
-  alignItemWithTrigger = true,
+  alignItemWithTrigger = false,
   ...props
 }: SelectPrimitive.Popup.Props &
   Pick<
