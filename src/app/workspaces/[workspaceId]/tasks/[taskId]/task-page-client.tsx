8b46b29b30219b350f5c362e01c414f31e86238e
jp-liu <fortune_coder@foxmail.com>
2026-05-14 17:54:05 +0800

    refactor(task): remove unused onFileOpen prop from terminal panel

--- src/app/workspaces/[workspaceId]/tasks/[taskId]/task-page-client.tsx
+++ src/app/workspaces/[workspaceId]/tasks/[taskId]/task-page-client.tsx
@@ -373,10 +373,6 @@ export function TaskPageClient({ task, workspaceId, workspaceName, latestExecuti
                   taskId={task.id}
                   worktreePath={activeWorktreePath}
                   onSessionEnd={handleSessionEnd}
-                  onFileOpen={(fullPath) => {
-                    setSelectedFilePath(fullPath);
-                    setActiveTab("files");
-                  }}
                 />
               </div>
             </div>
